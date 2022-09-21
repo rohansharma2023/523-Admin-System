5afb990f94ccd32e76d63be17a21c4d7db450896
dl805387 <59711497+dl805387@users.noreply.github.com>
2022-09-20 22:49:48 -0400

    implemented crud applications

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ const axios = require('axios').default;
 function App() {
     const [foodName, setFoodName] = useState("");
     const [days, setDays] = useState(0);
+    const [foodList, setFoodList] = useState([]);
+    const [newFoodName, setNewFoodName] = useState("");
 
     const addToList = () => {
         axios.post("http://localhost:3001/insert", {
@@ -13,6 +15,24 @@ function App() {
             days: days,
         });
     };
+
+    const updateFood = (id) => {
+        axios.put("http://localhost:3001/update", {
+            id: id,
+            newFoodName: newFoodName,
+        });
+    }
+
+    const deleteFood = (id) => {
+        axios.delete(`http://localhost:3001/delete/${id}`);
+    }
+
+    useEffect(()=>{
+        axios.get("http://localhost:3001/read").then((res)=>{
+            console.log(res);
+            setFoodList(res.data);
+        });
+    }, []);
 
     return (
         <div className="App">
@@ -27,6 +47,25 @@ function App() {
                 setDays(event.target.value);
             }}></input>
             <button onClick={addToList}>Add to list</button>
+        
+            <h1>Food List</h1>
+            {foodList.map((val, key)=>{
+                return (
+                    <div key="key" className="food">
+                        <h1>{val.foodName}</h1>
+                        <h1>{val.daysSinceIAte}</h1>
+                        <input 
+                            type="text" 
+                            placeholder="New Food Name ..." 
+                            onChange={(event)=>{
+                                setNewFoodName(event.target.value);
+                            }}>
+                        </input>
+                        <button onClick={()=>{updateFood(val._id)}}>Update</button>
+                        <button onClick={()=>{deleteFood(val._id)}}>Delete</button>
+                    </div>
+                );
+            })}
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -26,6 +26,36 @@ app.post("/insert", async(req, res) => {
     }
 });
 
+app.get("/read", async(req, res) => {
+    foodModel.find({}, (err, result)=>{
+        if (err) {
+            res.send(err);
+        }
+        res.send(result);
+    })
+});
+
+app.put("/update", async(req, res) => {
+    const newFoodName = req.body.newFoodName;
+    const id = req.body.id;
+    
+    try {
+        await foodModel.findById(id, (err, updatedFood)=>{
+            updatedFood.foodName = newFoodName;
+            updatedFood.save();
+            res.send("update");
+        });
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+app.delete("/delete/:id", async(req, res)=>{
+    const id = req.params.id;
+    await foodModel.findByIdAndRemove(id).exec();
+    res.send("deleted");
+});
+
 app.listen(3001, ()=>{
     console.log("server running on port 3001...");
 });
