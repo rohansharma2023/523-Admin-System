8ba30217fd884189c1ec241906e08fdc3102cd7d
dl805387 <59711497+dl805387@users.noreply.github.com>
2022-09-20 22:54:12 -0400

    implemented basic crud applications

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const app = express();
+const app = express(); 
 
 const foodModel = require("./models/Food");
 
