160cd032bd75c2f4561c9c2a6630914c5b27f664
aaronliang12 <aliang12@live.unc.edu>
2022-10-02 16:11:52 -0400

    Updated fields for client side form but still need to figure out how to submit fields to database

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,88 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
+
 const axios = require('axios').default;
 
 function App() {
-    const [foodName, setFoodName] = useState("");
-    const [days, setDays] = useState(0);
-    const [foodList, setFoodList] = useState([]);
-    const [newFoodName, setNewFoodName] = useState("");
+    const [title, setTitleName] = useState("");
+    const [description, setDescription] = useState("");
+    const [email, setEmail] = useState("");
+    const [date, setDate] = useState("");
+    const [status, setStatus] = useState("");
+    const [institution, setInstitution] = useState("");
+    const [name, setName] = useState("");
+    const [phone_number, setPhoneNumber] = useState("");
+    const [file, setFile] = useState("");
 
     const addToList = () => {
         axios.post("http://localhost:3001/insert", {
-            foodName: foodName, 
-            days: days,
+            title: title, 
+            description: description,
+            email: email,
+            date: date, 
+            status: status, 
+            institution: institution,
+            name: name, 
+            phone_number: phone_number, 
+            file: file, 
         });
     };
 
-    const updateFood = (id) => {
+    /* const updateFood = (id) => {
         axios.put("http://localhost:3001/update", {
             id: id,
             newFoodName: newFoodName,
         });
-    }
+    } */
 
-    const deleteFood = (id) => {
+    /* const deleteFood = (id) => {
         axios.delete(`http://localhost:3001/delete/${id}`);
-    }
+    } */
 
-    useEffect(()=>{
+   /*  useEffect(()=>{
         axios.get("http://localhost:3001/read").then((res)=>{
             console.log(res);
             setFoodList(res.data);
         });
-    }, []);
+    }, []);*/
 
     return (
         <div className="App">
-            <h1>crud app with mern</h1>
+            <h1>Client Proposal</h1>
 
-            <label>Food Name</label>
+            <label>Title</label>
             <input type="text" onChange={(event)=>{
-                setFoodName(event.target.value);
+                setTitleName(event.target.value);
             }}></input>
-            <label>Days since you ate it</label>
-            <input type="number" onChange={(event)=>{
-                setDays(event.target.value);
+            <label>Please give a description of your project</label>
+            <input type="text" onChange={(event)=>{
+                setDescription(event.target.value);
             }}></input>
-            <button onClick={addToList}>Add to list</button>
-        
-            <h1>Food List</h1>
-            {foodList.map((val, key)=>{
-                return (
-                    <div key="key" className="food">
-                        <h1>{val.foodName}</h1>
-                        <h1>{val.daysSinceIAte}</h1>
-                        <input 
-                            type="text" 
-                            placeholder="New Food Name ..." 
-                            onChange={(event)=>{
-                                setNewFoodName(event.target.value);
-                            }}>
-                        </input>
-                        <button onClick={()=>{updateFood(val._id)}}>Update</button>
-                        <button onClick={()=>{deleteFood(val._id)}}>Delete</button>
-                    </div>
-                );
-            })}
+            <label>Date</label>
+            <input type="text" onChange={(event)=>{
+                setDate(event.target.value);
+            }}></input>
+            <label>Status</label>
+            <input type="text" onChange={(event)=>{
+                setStatus(event.target.value);
+            }}></input>
+            <label>Please enter the name of your organization or institution</label>
+            <input type="text" onChange={(event)=>{
+                setInstitution(event.target.value);
+            }}></input>
+            <label>Please enter your name</label>
+            <input type="text" onChange={(event)=>{
+                setName(event.target.value);
+            }}></input>
+            <label>Please enter a valid Phone Number that we can contact</label>
+            <input type="text" onChange={(event)=>{
+                setPhoneNumber(event.target.value);
+            }}></input>
+            <label>Feel Free to add any files you deem important</label>
+            <input type="text" onChange={(event)=>{
+                setFile(event.target.value);
+            }}></input>
+            <button onClick={() =>{addToList()}}>Submit</button> 
         </div>
     );
 }
